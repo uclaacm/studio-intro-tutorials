7f177a7b4ba69316fbb6ff5a469b014d2c547231
Peter Sutarjo <starfox0412@yahoo.com>
2021-08-28 15:13:47 -0700

    simple camera (no rotation)

--- Roll a Ball/Assets/Scripts/CameraController.cs
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -6,12 +6,11 @@ public class CameraController : MonoBehaviour
 {
     public GameObject camTarget;
     public float posLerp = 0.04f;
-    public float rotLerp = 0.01f;
 
+    private 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = Vector3.Lerp(transform.position, camTarget.transform.position, posLerp);
-        transform.rotation = Quaternion.Lerp(transform.rotation, camTarget.transform.rotation, rotLerp);
     }
 }
