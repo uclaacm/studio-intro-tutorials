d2a2a375f5dda4993828de1d9298212a82991f97
Peter Sutarjo <peter.sutarjo@gmail.com>
2021-10-17 15:02:20 -0700

    added DontDestroyOnLoad script

---
+++ Roll a Ball/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("GameController");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
