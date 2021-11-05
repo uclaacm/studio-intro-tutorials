97c38bffcd44385d3f0f6653ff5f5de7cf6a093b
Richard Cheng <richard.hokan.cheng@gmail.com>
2021-11-04 17:41:54 -0700

    Fixed indentation

--- Platformer Part I/Assets/Scripts/DestructibleTilemap.cs
+++ Platformer Part I/Assets/Scripts/DestructibleTilemap.cs
@@ -21,50 +21,50 @@ public class DestructibleTilemap : MonoBehaviour
 	}
 
 	// Trigger tile deletion when entering the collider
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-    	TriggerDeletions(collision);
-    }
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		TriggerDeletions(collision);
+	}
 
-    // Trigger tile deletion when staying in collider, since CompositeCollider2D combines colliders of tiles
-    void OnCollisionStay2D(Collision2D collision)
-    {
-    	TriggerDeletions(collision);
-    }
+	// Trigger tile deletion when staying in collider, since CompositeCollider2D combines colliders of tiles
+	void OnCollisionStay2D(Collision2D collision)
+	{
+		TriggerDeletions(collision);
+	}
 
-    // Find position of tiles being collided with and trigger their deletion
-    private void TriggerDeletions(Collision2D collision)
-    {
-    	Vector3 tilePosition = Vector3.zero;
+	// Find position of tiles being collided with and trigger their deletion
+	private void TriggerDeletions(Collision2D collision)
+	{
+		Vector3 tilePosition = Vector3.zero;
 
-    	// Iterate over all points where the collider is touching something
-    	foreach (ContactPoint2D contact in collision.contacts)
-    	{
-    		tilePosition.x = contact.point.x + offset * contact.normal.x;	// Add small offset to move to correct tile
-    		tilePosition.y = contact.point.y + offset * contact.normal.y;
-    		StartCoroutine(DeleteTileDelayed(tilePosition));				// Start deletion of tile
-    	}
-    }
+		// Iterate over all points where the collider is touching something
+		foreach (ContactPoint2D contact in collision.contacts)
+		{
+			tilePosition.x = contact.point.x + offset * contact.normal.x;	// Add small offset to move to correct tile
+			tilePosition.y = contact.point.y + offset * contact.normal.y;
+			StartCoroutine(DeleteTileDelayed(tilePosition));				// Start deletion of tile
+		}
+	}
 
-    private IEnumerator DeleteTileDelayed(Vector3 tilePosition)
-    {
-    	// Check if tile is already being deleted
-    	if (!beingDeleted.Contains(tilePosition))
-    	{
+	private IEnumerator DeleteTileDelayed(Vector3 tilePosition)
+	{
+		// Check if tile is already being deleted
+		if (!beingDeleted.Contains(tilePosition))
+		{
 
-    		// Add tile to set of tiles being deleted
-    		beingDeleted.Add(tilePosition);
+			// Add tile to set of tiles being deleted
+			beingDeleted.Add(tilePosition);
 
-    		// Only need to wait if delay is non-negative
-    		if (deletionDelay > 0)
-    		{
-	    		yield return new WaitForSeconds(deletionDelay);
-	    	}
+			// Only need to wait if delay is non-negative
+			if (deletionDelay > 0)
+			{
+				yield return new WaitForSeconds(deletionDelay);
+			}
 
-	    	// Actually delete tile after delay
-	    	tilemap.SetTile(tilemap.WorldToCell(tilePosition), null);
-	    	// Remove tile from set of tiles being deleted
-	    	beingDeleted.Remove(tilePosition);
-	    }
-    }
+			// Actually delete tile after delay
+			tilemap.SetTile(tilemap.WorldToCell(tilePosition), null);
+			// Remove tile from set of tiles being deleted
+			beingDeleted.Remove(tilePosition);
+		}
+	}
 }
