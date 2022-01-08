488dd27d969baaaf8b8b270430a222c404ed60fb
Connor Daly <connord2838@gmail.com>
2022-01-08 14:39:24 -0800

    added project for winter tutorials

---
+++ Pokemon_test/Assets/Scripts/HpBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpBar : MonoBehaviour
+{
+    float maxHp;
+    // Update is called once per frame
+    void setHP(float hp)
+    {
+        this.transform.localScale = new Vector2(hp / maxHp, 1f);
+    }
+}
