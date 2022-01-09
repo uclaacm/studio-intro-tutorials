f668d4ba55ff3edd72ed2df17076b9e1bc7d4000
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-01-08 16:09:00 -0800

    InventoryTest scene

---
+++ Pokemon_test/Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/InventoryItem", order = 1)]
+public class InventoryItem : ScriptableObject
+{
+    [SerializeField] string idName;
+    [SerializeField] string displayName;
+    [SerializeField] [TextArea] string tooltip;
+    [SerializeField] Sprite icon;
+    
+
+    // PUBLIC 
+    public string GetIDName()
+    {
+        return idName;
+    }
+
+    public Sprite GetIcon()
+    {
+        return icon;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+}
