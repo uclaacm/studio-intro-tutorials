ebd1295d3c04ed75d8437dd707db215cf3087523
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-10-09 12:28:55 -0700

    Update CameraController.cs

--- Roll A Ball - Part I/Assets/CameraController.cs
+++ Roll A Ball - Part I/Assets/CameraController.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    // Reference to the player's Transform (their position)
     [SerializeField] private Transform player;
+
+    // Offsets for our camera
     [SerializeField] float yOffset = 1.5f;
     [SerializeField] float xOffset = 0f;
     [SerializeField] float zOffset = -2f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        ///
+        /// So we want the camera to follow the player, but at a distance. To do this we set our
+        /// camera's position to the player's position vector plus another vector containing an offset.
+        ///
         transform.position = player.position + new Vector3(xOffset, yOffset, zOffset);        
     }
 }
